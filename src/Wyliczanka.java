f16f19337c6fef8736b0f182e8d2b813a9b88b8c
blazej <wituckib@gmail.com>
2022-10-30 14:06:52 +0100

    Zadanie domowe 3

---
+++ src/Wyliczanka.java
@@ -0,0 +1,9 @@
+public class Wyliczanka {
+
+    public static void main(String[] args) {
+        for (int i=1;i<11;i=i+1 ) {
+            System.out.println(i);}//Jak jest i++ to liczyć ciągle a jak i=i+1 to do 19 :)
+        System.out.println("Działa :)");
+    }
+    }
+
